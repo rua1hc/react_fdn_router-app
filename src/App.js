c89c53a7aa8136241130ef2e38b00979d8e28cd2
rua1hc <tatut90@gmail.com>
2022-06-24 23:28:38 +0700

    s6v3-4 add Routing and Switch

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+import { Route, Routes, Switch } from "react-router-dom";
+
+import NavBar from "./components/navbar";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+
+                <div className="content">
+                    {/* <Routes> */}
+                    <Switch>
+                        <Route path="/products" component={Products} />
+                        <Route path="/posts" component={Posts} />
+                        <Route path="/admin" component={Dashboard} />
+                        <Route path="/" component={Home} />
+                        {/* <Route path="/" exact component={Home} /> */}
+                    </Switch>
+                    {/* </Routes> */}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
