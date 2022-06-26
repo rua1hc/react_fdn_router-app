e9cb86294a3e95716b7de42bfea692375f617840
rua1hc <tatut90@gmail.com>
2022-06-26 15:23:47 +0700

    s6v13 nested routing

--- src/components/admin/dashboard.jsx
+++ src/components/admin/dashboard.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+import { Route, Link } from "react-router-dom";
+
+import SideBar from "./sidebar";
+import Posts from "./posts";
+import Users from "./users";
+
 const Dashboard = ({ match }) => {
-  return (
-    <div>
-      <h1>Admin Dashboard</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            <SideBar />
+
+            <Route path="/admin/posts" component={Posts} />
+            <Route path="/admin/users" component={Users} />
+        </div>
+    );
 };
 
 export default Dashboard;
